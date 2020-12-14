b27e05e73a1c186db822323be89a1160121defdb
thai pham <jsxthai@gmail.com>
2020-12-14 22:43:42 +0700

    add router homepage and cart

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { AppBar, Toolbar, IconButton, Badge, Typography } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/img/Shopee-logo.png';
 import useStyles from './styles'
 
 const Narbar = ({ totalItems }) => {
     const classes = useStyles();
-    
+
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to='/' variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                         Shopaa
                     </Typography>
                     <div className={classes.grow}></div>
                     <div className={classes.button}>
                         <IconButton aria-label="Show card items" color="inherit">
-                            <Badge badgeContent={totalItems} color="secondary">
+                            <Badge component={Link} to='/cart' badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
