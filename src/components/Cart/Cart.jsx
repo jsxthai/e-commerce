795330cd903c60f9f05a94f1f57a3f20216b1b4a
thai pham <jsxthai@gmail.com>
2020-12-15 15:31:03 +0700

    action with cart items

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
-import useStyles from './styles';
-import CardItem from './CartItem/CartItem';
+import { Link } from 'react-router-dom';
 
-const Cart = ({ cart }) => {
+import CardItem from './CartItem/CartItem';
+import useStyles from './styles';
+
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant="subtitle1">You have no items in your cart !</Typography>
+        <Typography variant="subtitle1">You have no items in your cart !
+            <Link to='/' className={classes.link}> Start adding some</Link>.
+        </Typography>
     );
     
     console.log('cart', cart);
@@ -16,15 +20,15 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={6} md={4} key={item.id}>
-                        <CardItem item={item}/>
+                        <CardItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart}/>
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                     <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                     <div>
-                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
-                        <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
+                        <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
+                        <Button component={Link} to="/checkout" className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
                     </div>
             </div>
         </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { AppBar, Toolbar, IconButton, Badge, Typography } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/img/Shopee-logo.png';
 import useStyles from './styles'
 
 const Narbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
 
     return (
         <>
@@ -18,13 +19,14 @@ const Narbar = ({ totalItems }) => {
                         Shopaa
                     </Typography>
                     <div className={classes.grow}></div>
-                    <div className={classes.button}>
-                        <IconButton aria-label="Show card items" color="inherit">
-                            <Badge component={Link} to='/cart' badgeContent={totalItems} color="secondary">
-                                <ShoppingCart />
-                            </Badge>
-                        </IconButton>
-                    </div>
+                    {location.pathname === '/' && (
+                        <div className={classes.button}>
+                            <IconButton aria-label="Show card items" color="inherit">
+                                <Badge component={Link} to='/cart' badgeContent={totalItems} color="secondary">
+                                    <ShoppingCart />
+                                </Badge>
+                            </IconButton>
+                        </div>)}
                 </Toolbar>
             </AppBar>
         </>
